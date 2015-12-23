4db218fb1ccd1ab51429b8160e71ac16f38fab44
Padam Bhandari <Padam.Bhandari@datascan.com>
2015-12-23 15:34:21 -0600

    First Commit

---
+++ WindowsServiceDemo/ProjectInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    using System.ServiceProcess;
+
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        //private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        //{
+        //    new ServiceController(serviceInstaller1.ServiceName).Start();
+        //}
+    }
+}
---
+++ WindowsServiceDemo/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    using System.IO;
+
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            File.Create(AppDomain.CurrentDomain.BaseDirectory + "Start.txt");
+        }
+
+        protected override void OnStop()
+        {
+            File.Create(AppDomain.CurrentDomain.BaseDirectory + "Stop.txt");
+        }
+    }
+}
